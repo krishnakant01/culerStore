0e82b89ce4c9212b1e84154f1b0b2647f0e1d010
krishnakant01 <63720159+krishnakant01@users.noreply.github.com>
2022-02-20 19:03:50 +0530

    authentication pages added.

---
+++ signup.js
@@ -0,0 +1,27 @@
+const SignupPageLoginButton = document.querySelector("#signup-pg-btn-login");
+
+SignupPageLoginButton.addEventListener("click", () => {
+
+    window.location.href = "login.html";
+
+});
+
+const btnEye = document.querySelector(".btn-eye");
+const eyeIcon = document.querySelector(".btn-eye i");
+const input = document.querySelector("#password");
+
+let visibility = false;
+btnEye.addEventListener("click", () => {
+
+    if (visibility === false) {
+        eyeIcon.classList.remove("fa-eye-slash");
+        eyeIcon.classList.add("fa-eye");
+        input.setAttribute("type", "text");
+        visibility = true;
+    } else {
+        eyeIcon.classList.remove("fa-eye");
+        eyeIcon.classList.add("fa-eye-slash");
+        input.setAttribute("type", "password");
+        visibility = false;
+    }
+});
